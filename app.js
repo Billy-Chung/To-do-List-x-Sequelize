ee9890d32625415f3eee3ecd440aec5fd93ccb9e
鄭又誠 <zxcy9980@gmail.com>
2020-06-24 09:50:14 +0800

    home router init

--- app.js
+++ app.js
@@ -15,9 +15,7 @@ app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-    res.send('hello world')
-  })
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
